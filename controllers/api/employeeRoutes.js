8e65b7ff11d3d1fe7970abd9988757cbdcb35f52
Ody <melodymonyok@gmail.com>
2021-07-22 20:33:23 -0500

    Fixing the isMgr to is_manager.

--- controllers/api/employeeRoutes.js
+++ controllers/api/employeeRoutes.js
@@ -8,7 +8,7 @@ router.post('/signup', async (req, res) => {
     req.session.save(() => {
       req.session.userId = employeeData.dataValues.id;
       req.session.username = employeeData.dataValues.username;
-      req.session.mgr = employeeData.dataValues.isMgr;
+      req.session.mgr = employeeData.dataValues.is_manager;
       req.session.loggedIn = true;
 
       res.status(200).json(employeeData);
