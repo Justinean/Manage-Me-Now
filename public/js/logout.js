8684577bb11a4b237ea6ed837d463e34989fa748
Brian Palay <bpplaysguitar@hotmail.com>
2021-07-24 11:18:59 -0500

    logout js file

---
+++ public/js/logout.js
@@ -0,0 +1,15 @@
+const logOut = async () => {
+  const response = await fetch('/api/employee/logout', {
+    method: 'POST',
+    headers: { 
+      'Content-Type': 'application/json' },
+  });
+
+  if (response.ok) {
+    document.location.replace('/');
+  } else {
+    alert(response.statusText);
+  }
+};
+
+document.querySelector('#logout').addEventListener('click', logOut);
