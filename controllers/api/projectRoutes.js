4bfa17f8cceab71866f9f6db80ffb900d9a606c0
Justin Hoch <justinhoch22@gmail.com>
2021-07-24 11:00:34 -0500

    Added project/employee POST route

--- controllers/api/projectRoutes.js
+++ controllers/api/projectRoutes.js
@@ -14,3 +14,19 @@ router.post('/project', async (req, res) => {
     res.status(400).json(err);
   }
 });
+
+router.post('/project/employee', async (req, res) => {
+  try {
+    const projectData = await Project.findOne({where: {name: req.body.name}});
+    const employeeData = await Employee.findOne({where: {username: req.body.username}});
+    await ProjectEmployee.create({
+      project_id: projectData.dataValues.id,
+      employee_id: employeeData.dataValues.id
+    });
+    res.json(200).json(projectData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+module.exports = router;
