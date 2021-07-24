8c3e12241f40e324e11d83c81665700cf9eafc6e
Justin Hoch <justinhoch22@gmail.com>
2021-07-24 10:58:25 -0500

    Added project POST route

---
+++ controllers/api/projectRoutes.js
@@ -0,0 +1,16 @@
+const router = require('express').Router();
+const { Employee, Project, ProjectEmployee } = require('../../models');
+
+router.post('/project', async (req, res) => {
+  try {
+    const projectData = await Project.create(req.body.name);
+    const employeeData = await Employee.findOne({where: {username: req.body.username}});
+    await ProjectEmployee.create({
+      project_id: projectData.dataValues.id,
+      employee_id: employeeData.dataValues.id
+    });
+    res.json(200).json(projectData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
