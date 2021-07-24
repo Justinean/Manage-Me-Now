6a9f9af99c8fd6a25be1f48a6dde3ea518f311b0
Justin Hoch <justinhoch22@gmail.com>
2021-07-24 11:29:17 -0500

    Added default route to projectRoutes

--- controllers/api/projectRoutes.js
+++ controllers/api/projectRoutes.js
@@ -1,13 +1,12 @@
 const router = require('express').Router();
 const { Employee, Project, ProjectEmployee } = require('../../models');
 
-router.post('/project', async (req, res) => {
+router.post('/', async (req, res) => {
   try {
     const projectData = await Project.create(req.body.name);
-    const employeeData = await Employee.findOne({where: {username: req.body.username}});
     await ProjectEmployee.create({
       project_id: projectData.dataValues.id,
-      employee_id: employeeData.dataValues.id
+      employee_id: req.session.userId
     });
     res.json(200).json(projectData);
   } catch (err) {
@@ -15,7 +14,7 @@ router.post('/project', async (req, res) => {
   }
 });
 
-router.post('/project/employee', async (req, res) => {
+router.post('/employee', async (req, res) => {
   try {
     const projectData = await Project.findOne({where: {name: req.body.name}});
     const employeeData = await Employee.findOne({where: {username: req.body.username}});
