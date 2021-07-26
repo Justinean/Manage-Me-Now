2de3283e8de8587b3cdbe6b0963b62652a795789
Ody <melodymonyok@gmail.com>
2021-07-26 15:28:49 -0500

    Updated index.js to include the taskRoutes.

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 
 const employeeRoutes = require('./employeeRoutes');
 const projectRoutes = require('./projectRoutes');
+const taskRoutes = require('./taskRoutes');
 
 router.use('/employees', employeeRoutes);
 router.use('/projects', projectRoutes);
+router.use('/tasks', taskRoutes);
 
 module.exports = router;
