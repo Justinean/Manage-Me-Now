969179675907a82486a20316b5af6e109dc7796b
Justin Hoch <justinhoch22@gmail.com>
2021-07-26 00:21:51 -0500

    Added dashboard/:id get route

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -14,7 +14,14 @@ router.get('/', async (req, res) => {
 });
 
 router.get('/:id', async (req, res) => {
-
+    const projectEmployee = await ProjectEmployee.findAll({where: {employee_id: req.params.id}});
+    const projects = [];
+    const tasks = await Task.findAll({where: {employee_id: req.params.id}});
+    for (i in projectEmployee) {
+        const project = await Project.findAll({where: {id: projectEmployee[i].dataValues.id}});
+        projects.push(project);
+    };
+    res.render('dashboard', {projects, tasks, isMgr: req.session.mgr})
 })
 
 router.get('/newProject', async (req, res) => {
