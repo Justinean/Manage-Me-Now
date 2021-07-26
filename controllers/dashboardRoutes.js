abf335fa4bd09830ac8ea6a2a43ed1401a11bb78
Ody <melodymonyok@gmail.com>
2021-07-26 15:05:00 -0500

    Small edits to clean up code.

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -1,38 +1,99 @@
-const { ProjectEmployee, Project, Task } = require('../models');
-
 const router = require('express').Router();
+const { ProjectEmployee, Project, Task } = require('../models');
+const withAuth = require('../utils/auth');
 
-router.get('/', async (req, res) => {
-    const projectEmployee = await ProjectEmployee.findAll({where: {employee_id: req.session.userId}});
-    const projects = [];
-    const tasks = await Task.findAll({where: {employee_id: req.session.userId}});
+router.get('/', withAuth, async (req, res) => {
+    const projectEmployee = await ProjectEmployee.findAll({
+        where: {
+            employee_id: req.session.userId
+        }
+    });
+    const projectArray = [];
     for (i in projectEmployee) {
-        const project = await Project.findAll({where: {id: projectEmployee[i].dataValues.id}});
-        projects.push(project);
+        const projectData = await Project.findAll({
+            where: {
+                id: projectEmployee[i].dataValues.id
+            }
+        });
+        projectArray.push(projectData);
     };
-    res.render('dashboard', {projects, tasks, isMgr: req.session.mgr})
+    const projects = await projectArray.map((project) => project.get({ plain: true }));
+
+    const taskData = await Task.findAll({
+        where: {
+            employee_id: req.session.userId
+        }
+    });
+    const tasks = await taskData.map((task) => task.get({ plain: true }));
+
+    res.render('dashboard', {
+        projects,
+        tasks,
+        isMgr: req.session.mgr
+    });
 });
 
-router.get('/:id', async (req, res) => {
-    const projectEmployee = await ProjectEmployee.findAll({where: {employee_id: req.params.id}});
-    const projects = [];
-    const tasks = await Task.findAll({where: {employee_id: req.params.id}});
+router.get('/:id', withAuth, async (req, res) => {
+    const projectEmployee = await ProjectEmployee.findAll({
+        where: {
+            employee_id: req.params.id
+        }
+    });
+    const projectArray = [];
     for (i in projectEmployee) {
-        const project = await Project.findAll({where: {id: projectEmployee[i].dataValues.id}});
-        projects.push(project);
+        const projectData = await Project.findAll({
+            where: {
+                id: projectEmployee[i].dataValues.id
+            }
+        });
+        projectArray.push(projectData);
     };
-    res.render('dashboard', {projects, tasks, isMgr: req.session.mgr})
-})
+    const projects = await projectArray.map((project) => project.get({ plain: true }));
+
+    const taskData = await Task.findAll({
+        where: {
+            employee_id: req.params.id
+        }
+    });
+    const tasks = await taskData.map((task) => task.get({ plain: true }));
+
+    res.render('dashboard', {
+        projects,
+        tasks,
+        isMgr: req.session.mgr
+    });
+});
 
 router.get('/newProject', async (req, res) => {
-    const projectEmployee = await ProjectEmployee.findAll({where: {employee_id: req.session.userId}});
-    const projects = [];
-    const tasks = await Task.findAll({where: {employee_id: req.session.userId}});
+    const projectEmployee = await ProjectEmployee.findAll({
+        where: {
+            employee_id: req.session.userId
+        }
+    });
+    const projectArray = [];
     for (i in projectEmployee) {
-        const project = await Project.findAll({where: {id: projectEmployee[i].dataValues.id}});
-        projects.push(project);
+        const projectData = await Project.findAll({
+            where: {
+                id: projectEmployee[i].dataValues.id
+            }
+        });
+        projectArray.push(projectData);
     };
-    res.render('dashboard', {projects, tasks, isMgr: req.session.mgr, newProject: true})
-})
+    const projects = await projectArray.map((project) => project.get({ plain: true }));
+
+    const taskData = await Task.findAll({
+        where: {
+            employee_id: req.session.userId
+        }
+    });
+    const tasks = await taskData.map((task) => task.get({ plain: true }));
+
+    res.render('dashboard', {
+        projects,
+        tasks,
+        newProject: true,
+        isMgr: req.session.mgr
+    });
+});
 
 module.exports = router;
