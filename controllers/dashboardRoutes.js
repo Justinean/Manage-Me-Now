161abc33ff6cdd8373ac3fa9dde50504fc1c2eb5
Justin Hoch <justinhoch22@gmail.com>
2021-07-26 00:16:09 -0500

    Added dashboard get route

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -1,4 +1,17 @@
+const { ProjectEmployee, Project, Task } = require('../models');
+
 const router = require('express').Router();
+
+router.get('/', async (req, res) => {
+    const projectEmployee = await ProjectEmployee.findAll({where: {employee_id: req.session.userId}});
+    const projects = [];
+    const tasks = await Task.findAll({where: {employee_id: req.session.userId}});
+    for (i in projectEmployee) {
+        const project = await Project.findAll({where: {id: projectEmployee[i].dataValues.id}});
+        projects.push(project);
+    };
+    res.render('dashboard', {projects, tasks, isMgr: req.session.mgr})
+});
 
 router.get('/:id', async (req, res) => {
 
