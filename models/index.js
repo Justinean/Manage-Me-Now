0e5c766ab81e0043cc0e4c4af7f5c671f88e5bb6
Justin Hoch <justinhoch22@gmail.com>
2021-07-26 00:18:18 -0500

    Added employee id to Task model

--- models/index.js
+++ models/index.js
@@ -6,6 +6,8 @@ const ProjectEmployee = require('./ProjectEmployee');
 
 Task.belongsTo(Project, { foreignKey: 'project_id', onDelete: "CASCADE", onUpdate: "CASCADE" });
 Project.hasMany(Task, { foreignKey: 'project_id' });
+Task.belongsTo(Employee, { foreignKey: 'employee_id', onDelete: "CASCADE", onUpdate: "CASCADE" });
+Employee.hasMany(Task, { foreignKey: 'employee_id'})
 Project.belongsToMany(Employee, { through: ProjectEmployee });
 Employee.belongsToMany(Project, { through: ProjectEmployee });
 
