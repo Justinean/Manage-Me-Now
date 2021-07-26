832c26a1e280cfc2872c241437188a5b8ac3bed8
Ody <melodymonyok@gmail.com>
2021-07-26 17:08:37 -0500

    Updated dashboard, just so i could see the page for testing purposes.

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -2,35 +2,43 @@ const router = require('express').Router();
 const { ProjectEmployee, Project, Task } = require('../models');
 const withAuth = require('../utils/auth');
 
-router.get('/', withAuth, async (req, res) => {
-    const projectEmployee = await ProjectEmployee.findAll({
-        where: {
-            employee_id: req.session.userId
-        }
-    });
-    const projectArray = [];
-    for (i in projectEmployee) {
-        const projectData = await Project.findAll({
+router.get('/', /* withAuth, */ async (req, res) => {
+    try {
+        console.log("Trying to get the dashboard.")
+        const projectEmployee = await ProjectEmployee.findAll({
             where: {
-                id: projectEmployee[i].dataValues.id
+                employee_id: req.session.userId
             }
         });
-        projectArray.push(projectData);
-    };
-    const projects = await projectArray.map((project) => project.get({ plain: true }));
+        const projectArray = [];
+        for (i in projectEmployee) {
+            const projectData = await Project.findOne({
+                where: {
+                    id: projectEmployee[i].dataValues.id
+                }
+            });
+            projectArray.push(projectData);
+        };
+        const projects = await projectArray.map((project) => project.get({ plain: true }));
 
-    const taskData = await Task.findAll({
-        where: {
-            employee_id: req.session.userId
-        }
-    });
-    const tasks = await taskData.map((task) => task.get({ plain: true }));
+        const taskData = await Task.findAll({
+            where: {
+                employee_id: req.session.userId
+            }
+        });
+        const tasks = await taskData.map((task) => task.get({ plain: true }));
 
-    res.render('dashboard', {
-        projects,
-        tasks,
-        isMgr: req.session.mgr
-    });
+        res.render('dashboard', {
+            projects,
+            tasks,
+            isMgr: req.session.mgr,
+            loggedIn: req.session.loggedIn,
+            username: req.session.username
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(400).json(err);
+    }
 });
 
 router.get('/:id', withAuth, async (req, res) => {
