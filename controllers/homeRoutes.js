c4beae0bedd85ba659b20995d19169898d9af6bd
Brian Palay <bpplaysguitar@hotmail.com>
2021-07-27 00:02:13 -0500

    added and worked on home and employee-list handlebars

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,23 +3,28 @@ const { Project, Employee } = require('../models');
 const withAuth = require('../utils/auth');
 
 router.get('/', withAuth, async (req, res) => {
-  const projects = await Project.findAll();
-  const managers = await Employee.findAll({
-    where: {
-      is_manager: true
-    }
-  });
-  const employees = await Employee.findAll({
-    where: {
-      is_manager: false
-    }
-  });
+const projectData = await Project.findAll();
+const projects = await projectData.map((project) => project.get({ plain: true }));
 
+const managerData = await Employee.findAll({
+  where: {
+    is_manager: true
+  }
+});
+const managers = await managerData.map((manager) => manager.get({ plain: true }));
+
+const employeeData = await Employee.findAll({
+  where: {
+    is_manager: false
+  }
+});
+const employees = await employeeData.map((employee) => employee.get({ plain: true })); 
 
   res.render('home', {
     projects,
     managers,
     employees,
+    loggedIn: req.session.loggedIn,
     isMgr: req.session.mgr
   });
 });
