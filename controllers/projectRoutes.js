dcaa46e5a2a77abb7165e4d4e97c5ab0446601c8
Ody <melodymonyok@gmail.com>
2021-07-27 16:37:09 -0500

    Added route for displaying the project page with the add employee data.

--- controllers/projectRoutes.js
+++ controllers/projectRoutes.js
@@ -2,7 +2,9 @@ const router = require('express').Router();
 const { Employee, Project, ProjectEmployee, Task } = require('../models');
 const withAuth = require('../utils/auth');
 
+// This route will render the currently requested project.
 router.get('/:id', withAuth, async (req, res) => {
+  // This gets all projects for the logged in Employee.
   const projectEmployee = await ProjectEmployee.findAll({
     where: {
       employee_id: req.session.userId
@@ -19,6 +21,15 @@ router.get('/:id', withAuth, async (req, res) => {
   };
   const projects = await projectArray.map((project) => project.get({ plain: true }));
 
+  // This will get the data for the current project.
+  const currentProjectData = await Project.findOne({
+    where: {
+      id: req.params.id
+    }
+  });
+  const currentProject = await currentProjectData.map((project) => project.get({ plain: true }));
+
+  // This gets all employees currently assigned to the project.
   const employeesOnProject = await ProjectEmployee.findAll({
     where: {
       project_id: req.params.id
@@ -35,6 +46,7 @@ router.get('/:id', withAuth, async (req, res) => {
   };
   const employees = await employeeArray.map((employee) => employee.get({ plain: true }));
 
+  // This will get all tasks on the current project.
   const taskData = await Task.findAll({
     where: {
       project_id: req.params.id
@@ -43,9 +55,78 @@ router.get('/:id', withAuth, async (req, res) => {
   const tasks = await taskData.map((task) => task.get({ plain: true }));
 
   res.render('project', {
+    currentProject,
     employees,
     projects,
     tasks,
+    loggedIn: req.session.loggedIn,
+    isMgr: req.session.mgr
+  });
+});
+
+router.get('/addEmployee/:id', withAuth, async (req, res) => {
+  // This gets all projects for the logged in Employee.
+  const projectEmployee = await ProjectEmployee.findAll({
+    where: {
+      employee_id: req.session.userId
+    }
+  });
+  const projectArray = [];
+  for (i in projectEmployee) {
+    const projectData = await Project.findOne({
+      where: {
+        id: projectEmployee[i].dataValues.project_id
+      }
+    });
+    projectArray.push(projectData);
+  };
+  const projects = await projectArray.map((project) => project.get({ plain: true }));
+
+  // This will get the data for the current project.
+  const currentProjectData = await Project.findOne({
+    where: {
+      id: req.params.id
+    }
+  });
+  const currentProject = await currentProjectData.map((project) => project.get({ plain: true }));
+
+  // This gets all employees currently assigned to the project.
+  const employeesOnProject = await ProjectEmployee.findAll({
+    where: {
+      project_id: req.params.id
+    }
+  });
+  const employeeArray = [];
+  for (x in employeesOnProject) {
+    const employeeData = await Employee.findOne({
+      where: {
+        id: employeesOnProject[x].dataValues.employee_id
+      }
+    });
+    employeeArray.push(employeeData);
+  };
+  const employees = await employeeArray.map((employee) => employee.get({ plain: true }));
+
+  // This gets all employees in the database.
+  const allEmployeeData = await Employee.findAll();
+  const allEmployees = await allEmployeeData.map((all) => all.get({ plain: true }));
+
+  // This will get all tasks on the current project.
+  const taskData = await Task.findAll({
+    where: {
+      project_id: req.params.id
+    }
+  });
+  const tasks = await taskData.map((task) => task.get({ plain: true }));
+
+  res.render('project', {
+    allEmployees,
+    currentProject,
+    employees,
+    projects,
+    tasks,
+    addEmployee: true,
+    loggedIn: req.session.loggedIn,
     isMgr: req.session.mgr
   });
 });
